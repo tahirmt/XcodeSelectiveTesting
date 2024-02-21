12452374a35aea2af3e3ba3afce03903dc0eb836
Mahmood Tahir <mahmood.tahir@thescore.com>
2024-02-21 14:21:53 -0500

    Add space instead of ..

--- Sources/Git/Git+Changeset.swift
+++ Sources/Git/Git+Changeset.swift
@@ -7,42 +7,45 @@ import PathKit
 import SelectiveTestLogger
 import SelectiveTestShell
 
-extension Git {    
+extension Git {
     public func changeset(baseBranch: String, verbose: Bool = false) throws -> Set<Path> {
         let gitRoot = try repoRoot()
-        
+
         var currentBranch = try Shell.execOrFail("cd \(gitRoot) && git branch --show-current").trimmingCharacters(in: .newlines)
         if verbose {
             Logger.message("Current branch: \(currentBranch)")
             Logger.message("Base branch: \(baseBranch)")
         }
-        
+        let separator: String
         if currentBranch.isEmpty {
             Logger.warning("Missing current branch at \(path)")
-            
+
             currentBranch = "HEAD"
+            separator = " "
+        } else {
+            separator = ".."
         }
-        
-        let changes = try Shell.execOrFail("cd \(path) && git diff '\(baseBranch)'..'\(currentBranch)' --name-only")
+
+        let changes = try Shell.execOrFail("cd \(path) && git diff '\(baseBranch)'\(separator)'\(currentBranch)' --name-only")
         let changesTrimmed = changes.trimmingCharacters(in: .whitespacesAndNewlines)
-        
+
         guard !changesTrimmed.isEmpty else {
             return Set()
         }
-        
+
         return Set(changesTrimmed.components(separatedBy: .newlines).map { gitRoot + $0 } )
     }
-    
+
     public func localChangeset() throws -> Set<Path> {
         let gitRoot = try repoRoot()
-        
+
         let changes = try Shell.execOrFail("cd \(gitRoot) && git diff --name-only")
         let changesTrimmed = changes.trimmingCharacters(in: .whitespacesAndNewlines)
-        
+
         guard !changesTrimmed.isEmpty else {
             return Set()
         }
-        
+
         return Set(changesTrimmed.components(separatedBy: .newlines).map { gitRoot + $0 } )
     }
 }
